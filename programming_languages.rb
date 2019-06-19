a93c9a9740d4de94f768df997fff08753e89294e
Samantha Kane <s.kane133@gmail.com>
2019-06-19 20:05:22 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -7,12 +7,23 @@ def reformat_languages(languages)
        new_hash[lan] ||= info
       new_hash[lan][:style] = []
     new_hash[lan][:style] << name
-    if lan == :javescript
-      new_hash[lan][:javascript][:style] << :oo
   end 
 end
 end
 new_hash
 end
 
-
+def reformat_languages(languages)
+  new_languages = {}
+  languages.each do |style, language|
+    language.each do |language_name, attribute|
+      if new_languages[language_name] == nil
+          new_languages[language_name] = attribute
+        end
+      new_languages[language_name][:style] = []
+      new_languages[language_name][:style] << style
+    end
+  end
+  new_languages[:javascript][:style] << :oo
+  new_languages
+end
