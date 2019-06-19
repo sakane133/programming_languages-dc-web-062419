4dfccda6fa75122beeea1018e1e2be4c2ad2293f
Samantha Kane <s.kane133@gmail.com>
2019-06-19 19:50:40 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,7 +3,7 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each do |name, type|
     type.each do |lan, info|
-       new_languages[name] ||= attributes
+       new_languages[name] ||= info
       new_hash[name][:style] = []
     nenw_hash[name][:style] << name
 end
@@ -11,13 +11,4 @@ end
 new_hash
 end
 
-
-languages.each do |style, language|
-  language.each do |name, attributes|
-  new_languages[name] ||= attributes
-  new_languages[name][:style]=[]
-  new_languages[name][:style]<<style
-end
-end
-new_languages
-end
+  
