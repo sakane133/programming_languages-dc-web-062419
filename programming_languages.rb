d1e85e6dbeca1f93421c4eb7a19f66f496f9e532
Samantha Kane <s.kane133@gmail.com>
2019-06-19 20:03:12 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -8,9 +8,7 @@ def reformat_languages(languages)
       new_hash[lan][:style] = []
     new_hash[lan][:style] << name
     if lan == :javescript
-      js = new_hash[lan][:javascript][:style] << [:functional, :oo]
-      retrun js
-  
+      new_hash[lan][:javascript][:style] << :oo
   end 
 end
 end
