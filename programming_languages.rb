2bcd63232f344281e293915d6a9fe75708ddef66
Samantha Kane <s.kane133@gmail.com>
2019-06-19 19:42:16 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,7 +4,7 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each do |name, type|
     type.each do |lan, info|
-      new_hash[:style]
+      new_hash[name][:style] = []
 end
 end
 end
