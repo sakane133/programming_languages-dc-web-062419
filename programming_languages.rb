f27f08df22ff730223ae50bb55d4f8631a7af0ad
Samantha Kane <s.kane133@gmail.com>
2019-06-19 20:06:44 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,12 +1,7 @@
-
-
 def reformat_languages(languages)
   new_languages = {}
   languages.each do |style, language|
     language.each do |language_name, attribute|
-      if new_languages[language_name] == nil
-          new_languages[language_name] = attribute
-        end
       new_languages[language_name][:style] = []
       new_languages[language_name][:style] << style
     end
